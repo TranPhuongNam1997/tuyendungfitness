e03c51684312377b2d54ab400c0ab85f32c61872
TranPhuongNam <tranphuongnam17041997@gmail.com>
2021-06-29 17:47:10 +0700

    update

--- libs/js/script.js
+++ libs/js/script.js
@@ -19,6 +19,34 @@ $( document ).ready(function() {
 
 });
 
+$(function() {
+
+    $('.ripple').on('click', function(event) {
+        event.preventDefault();
+        var $btn = $(this),
+            $div = $('<div/>'),
+            btnOffset = $btn.offset(),
+            xPos = event.pageX - btnOffset.left,
+            yPos = event.pageY - btnOffset.top;
+
+        $div.addClass('ripple-effect');
+        $div
+            .css({
+                height: $btn.height(),
+                width: $btn.height(),
+                top: yPos - ($div.height() / 2),
+                left: xPos - ($div.width() / 2),
+                background: $btn.data("ripple-color") || "#fff"
+            });
+        $btn.append($div);
+
+        window.setTimeout(function() {
+            $div.remove();
+        }, 2000);
+    });
+
+});
+
 $(document).ready(function (event) {
 
     $('.showhide-pass div').click(function () {
@@ -34,6 +62,19 @@ $(document).ready(function (event) {
     $('.btn-category').click(function () {
        $('.navigation').slideToggle(300,'swing');
     });
-
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function() {
+        readURL(this);
+    });
 
 });
